169fd8f8f3de53a84613c5c57e8694e6fb7bb28c
Alexander Koch <alexander@4koch.de>
2019-03-03 16:53:37 +0100

    Fix bugs in algorithms

--- Main.py
+++ Main.py
@@ -5,39 +5,45 @@ import random
 from Experience import Experience
 from Models import simple_DNN
 import math
-
+import MathAI
 env = gym.make('MountainCar-v0')
 observation = env.reset()
 
-eta = 0.9
-gamma = 0.95
-eta_discount = 0.9995
+eta = 0.0
+gamma = 0.9
+eta_discount = 1.0#0.9995
+rpe_scalar = 0.5
 input_length = env.observation_space.shape[0]
 output_length = 3
-online_network = simple_DNN(input_length, output_length, weight_mean=0.5)
-target_network = simple_DNN(input_length, output_length, weight_mean=0.5)
-rpe_network = simple_DNN(input_length, 1)
+online_network = simple_DNN(architecture=[input_length,  20, 20, output_length], weight_mean=0.0)
+target_network = simple_DNN(architecture=[input_length, 20, 20, output_length], weight_mean=0.0)
+rpe_network = simple_DNN(architecture=[input_length, 10, input_length], weight_mean=0.5)
 memory = Experience(10000)
 
 tf_eta_summary = tf.Summary()
-tf_steps_summary = tf.Summary()
-tf_steps_summary.value.add(tag="steps", simple_value=None)
-tf_eta_summary.value.add(tag="eta", simple_value=0)
+tf_reward_summary = tf.Summary()
+tf_rpe_summary = tf.Summary()
+tf_reward_summary.value.add(tag="Reward sum", simple_value=None)
+tf_eta_summary.value.add(tag="Max distance", simple_value=0)
+tf_rpe_summary.value.add(tag="Reward prediction error", simple_value=0)
 
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    summary_writer = tf.summary.FileWriter("./logs/MC_DDQN_9", sess.graph)
+    summary_writer = tf.summary.FileWriter("./logs/MC_2", sess.graph)
 
-    for episode in range(2000):
+    for episode in range(20000):
         observation = env.reset()
         eta *= eta_discount
+        rpe_scalar *= 1.1
         reward_sum = 0
+        max_distance = -0.5
         for step in range(1000):
             # env.render()
+            rpe_scalar *= 1.001
             q, q_summary = online_network.predict(sess, observation, summary=True)
-            reward_prediciton = rpe_network.predict(sess, observation)
-            print("rpe: " + str(reward_prediciton[0]))
+            state_prediciton = rpe_network.predict(sess, observation)
+            #print("rpe: " + str(reward_prediciton[0]))
             summary_writer.add_summary(q_summary, episode * 200 + step)
             if random.randint(0, 100) < (eta*100):
                 action = env.action_space.sample()
@@ -45,24 +51,27 @@ with tf.Session() as sess:
                 action = np.argmax(q)
 
             observation_next, reward, done, _ = env.step(action)
-
-            rpe_network.train(sess, observation, [reward])
-            rpe = math.pow(reward_prediciton[0] - reward, 2)
-            reward = reward + rpe
-            print(reward)
+            max_distance = max(max_distance, observation[0])
             reward_sum += reward
+            rpe_network.train(sess, observation, observation_next)
+            rpe = MathAI.sigmoid(MathAI.euclidean_distance(np.array(state_prediciton)[0], observation_next))
+            tf_rpe_summary.value[0].simple_value = rpe
+            summary_writer.add_summary(tf_rpe_summary, episode * 200 + step)
+            reward = reward + math.fabs(observation[0] + 0.5)
+            #print(reward)
+
             priority = rpe
             memory.store(observation, action, reward, observation_next, done, priority)
             observation = observation_next
             if done:
-                tf_eta_summary.value[0].simple_value = eta
+                tf_eta_summary.value[0].simple_value = max_distance
                 summary_writer.add_summary(tf_eta_summary, episode)
-                tf_steps_summary.value[0].simple_value = reward_sum
-                summary_writer.add_summary(tf_steps_summary, episode)
+                tf_reward_summary.value[0].simple_value = reward_sum
+                summary_writer.add_summary(tf_reward_summary, episode)
                 break
 
-        for i in range(20):
-            [observation, action, reward, observation_next, done, priority], index = memory.sample(prioritized_replay=True)
+        for i in range(1):
+            [observation, action, reward, observation_next, done, priority], index = memory.sample(prioritized_replay=False)
             q_state = online_network.predict(sess, observation)
             target = np.copy(q_state[0])
 
--- Models.py
+++ Models.py
@@ -3,24 +3,44 @@ import tensorflow as tf
 
 class simple_DNN():
 
-    def __init__(self, input_length, output_length, weight_mean=0.0):
-        self.tf_x = tf.placeholder(shape=(input_length), dtype=tf.float32, name="x")
-        self.tf_y = tf.placeholder(shape=(output_length), dtype=tf.float32, name="y")
+    def __init__(self, architecture, weight_mean=0.0):
+        self.tf_x = tf.placeholder(shape=(architecture[0]), dtype=tf.float32, name="x")
+        self.tf_y = tf.placeholder(shape=(architecture[-1]), dtype=tf.float32, name="y")
 
-        self.l0 = tf.transpose(tf.expand_dims(self.tf_x, axis=1))
-        self.w1 = tf.Variable(tf.truncated_normal(shape=(input_length, 20), mean=weight_mean))
-        self.b1 = tf.Variable(tf.truncated_normal(shape=(1, 20), mean=weight_mean))
-        self.l1 = tf.nn.relu(tf.add(tf.matmul(self.l0, self.w1), self.b1))
+        """self.l0 = tf.transpose(tf.expand_dims(self.tf_x, axis=1))
+        self.w1 = tf.Variable(tf.truncated_normal(shape=(input_length, architecture[0]), mean=weight_mean))
+        self.b1 = tf.Variable(tf.truncated_normal(shape=(1, architecture[0]), mean=weight_mean))
+        self.l1 = tf.nn.relu(tf.add(tf.matmul(self.l0, self.w1), self.b1))"""
 
-        self.w2 = tf.Variable(tf.truncated_normal(shape=(20, 20), mean=weight_mean))
+        self.layer_outputs = []
+        self.layer_weights = []
+        self.layer_biases = []
+
+        for layer in range(len(architecture) - 1):
+            # input layer
+            weights = tf.Variable(
+                tf.truncated_normal(shape=(architecture[layer], architecture[layer + 1]), mean=weight_mean))
+            biases = tf.Variable(tf.truncated_normal(shape=(1, architecture[layer + 1]), mean=weight_mean))
+            if layer == 0:
+                l0 = tf.transpose(tf.expand_dims(self.tf_x, axis=1))
+                output = tf.nn.relu(tf.add(tf.matmul(l0, weights), biases))
+            elif layer == len(architecture) - 2:
+                output = tf.add(tf.matmul(self.layer_outputs[layer - 1], weights), biases)
+            else:
+                output = tf.nn.relu(tf.add(tf.matmul(self.layer_outputs[layer - 1], weights), biases))
+
+            self.layer_weights.append(weights)
+            self.layer_biases.append(biases)
+            self.layer_outputs.append(output)
+
+        """self.w2 = tf.Variable(tf.truncated_normal(shape=(20, 20), mean=weight_mean))
         self.b2 = tf.Variable(tf.truncated_normal(shape=(1, 20), mean=weight_mean))
         self.l2 = tf.nn.relu(tf.add(tf.matmul(self.l1, self.w2), self.b2))
 
         self.w3 = tf.Variable(tf.truncated_normal(shape=(20, output_length), mean=weight_mean))
-        self.b3 = tf.Variable(tf.truncated_normal(shape=(1, output_length), mean=weight_mean))
+        self.b3 = tf.Variable(tf.truncated_normal(shape=(1, output_length), mean=weight_mean))"""
 
-
-        self.tf_out = tf.reshape(tf.add(tf.matmul(self.l2, self.w3), self.b3), shape=(output_length, ))
+        self.tf_out = tf.reshape(self.layer_outputs[-1], shape=(architecture[-1], ))
 
         self.tf_loss = tf.losses.mean_squared_error(self.tf_y, self.tf_out)
         self.optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.tf_loss)
@@ -43,12 +63,15 @@ class simple_DNN():
 
         if self.copy_operation is None:
             ops = []
-            ops.append(self.w1.assign(source_dnn.w1))
+            for layer in range(len(self.layer_weights)):
+                ops.append(self.layer_weights[layer].assign(source_dnn.layer_weights[layer]))
+                ops.append(self.layer_biases[layer].assign(source_dnn.layer_biases[layer]))
+            """ops.append(self.w1.assign(source_dnn.w1))
             ops.append(self.w2.assign(source_dnn.w2))
             ops.append(self.w3.assign(source_dnn.w3))
             ops.append(self.b1.assign(source_dnn.b1))
             ops.append(self.b2.assign(source_dnn.b2))
-            ops.append(self.b3.assign(source_dnn.b3))
+            ops.append(self.b3.assign(source_dnn.b3))"""
 
             self.copy_operation = tf.group(ops)
 
