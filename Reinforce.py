44b14c47459a3359f43d92ec69d3e051fdb80a83
Alexander Koch <alexander@4koch.de>
2019-03-21 17:15:14 +0100

    new version

--- Reinforce.py
+++ Reinforce.py
@@ -7,7 +7,7 @@ env = gym.make('CartPole-v0')
 
 tf_input = tf.placeholder(dtype=tf.float32, shape=(4,))
 tf_return = tf.placeholder(dtype=tf.float32)
-tf_actions = tf.placeholder(dtype=tf.float32, shape=(2,))
+tf_action = tf.placeholder(dtype=tf.int32)
 
 l0 = tf.transpose(tf.expand_dims(tf_input, axis=1))
 tf_w1 = tf.Variable(tf.truncated_normal(shape=(4, 10)))
@@ -18,13 +18,18 @@ tf_w2 = tf.Variable(tf.truncated_normal(shape=(10, 2)))
 tf_b2 = tf.Variable(tf.truncated_normal(shape=(2,)))
 tf_output = tf.nn.softmax(tf.add(tf.matmul(tf_l1, tf_w2), tf_b2))
 
-tf_log_output = tf.math.multiply(tf.log(tf_output), tf_actions)
+tf_log_output = tf.math.multiply(tf.log(tf_output), tf.one_hot(tf_action, on_value=tf_return, depth=2))
 loss = -tf.reduce_sum(tf_log_output)
 
-tf_optimizer = tf.train.AdamOptimizer(learning_rate=0.0005).minimize(loss)  # -tf_log_output * tf_return)
+tf_optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)  # -tf_log_output * tf_return)
+
+tf_summary_steps = tf.Summary()
+tf_summary_steps.value.add(tag="steps", simple_value=None)
+
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    summary_writer = tf.summary.FileWriter("./logs/CP_RE_2")
     for episode in range(10000):
         # env.render()
         state = env.reset()
@@ -39,7 +44,9 @@ with tf.Session() as sess:
             trajectory.append([state, action, reward, next_state])
             state = next_state
             if done:
-                print(step)
+                print(step + 1)
+                tf_summary_steps.value[0].simple_value = step + 1
+                summary_writer.add_summary(tf_summary_steps, episode)
                 break
 
         episode_return = 0
@@ -52,9 +59,5 @@ with tf.Session() as sess:
         episode_return = state_value
         for state, action, reward, next_state in trajectory:
             baseline = episode_return - average_reward
-            if action == 0:
-                actions = [baseline, 0]
-            else:
-                actions = [0, baseline]
-            _, l1 = sess.run([tf_optimizer, tf_l1], feed_dict={tf_input: state, tf_return: baseline, tf_actions: actions})
+            _, l1 = sess.run([tf_optimizer, tf_l1], feed_dict={tf_input: state, tf_return: baseline, tf_action: action})
             episode_return -= reward
